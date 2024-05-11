37f983e2b43330b178627070e4e932bb0dec2e61
atree4728 <atree.public@gmail.com>
2024-05-11 07:15:17 +0000

    Add unary operator +, -

--- src/atrcc.c
+++ src/atrcc.c
@@ -129,7 +129,8 @@ Node *new_node_num(int val) {
 
 /* EBNF:
  *   expr    = mul | (("+" | "-") mul)*
- *   mul     = primary | (("*" | "/") primary)*
+ *   mul     = unary | (("*" | "/") unary)*
+ *   unary   = ("+" | "-")? primary
  *   primary = num | "(" expr ")"
  */
 
@@ -143,13 +144,21 @@ Node *primary() {
     return new_node_num(expect_number());
 }
 
+Node *unary() {
+    if (consume('+'))
+        return primary();
+    if (consume('-'))
+        return new_node(ND_SUB, new_node_num(0), primary());
+    return primary();
+}
+
 Node *mul() {
-    Node *node = primary();
+    Node *node = unary();
     while (true) {
         if (consume('*'))
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         else if (consume('/'))
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         else
             return node;
     }
--- test/test.sh
+++ test/test.sh
@@ -4,7 +4,7 @@ assert() {
 	expected="$1"
 	input="$2"
 
-	eval "./atrcc $input > tmp.s"
+	eval "./atrcc \"$input\" > tmp.s"
 	eval "cc -o tmp tmp.s"
 	eval "./tmp"
 	actual="$?"
@@ -19,9 +19,11 @@ assert() {
 
 assert 0 "0"
 assert 21 "5+20-4"
-assert 41 "\ \ 12\ +\ 34-\ \ \ \ 5\ \ \ \ "
+assert 41 "  12 + 34-    5    "
 assert 47 "5+6*7"
-assert 15 "5*\(9-6\)"
-assert 4 "\(3+5\)/2"
+assert 15 "5*(9-6)"
+assert 4 "(3+5)/2"
+assert 10 "-10+20"
+assert 10 "-(-2*(10/+2))"
 
 echo OK

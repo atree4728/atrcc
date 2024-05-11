bdb598513c613e63c0c2f2746799c1cfef0cf80c
atree4728 <atree.public@gmail.com>
2024-05-11 07:03:49 +0000

    Add mul, div, operator precednce

--- src/atrcc.c
+++ src/atrcc.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <ctype.h>
 #include <stdarg.h>
 #include <stdbool.h>
@@ -86,7 +87,8 @@ Token *tokenize() {
     while (*p) {
         if (isspace(*p)) {
             p++;
-        } else if (*p == '+' || *p == '-') {
+        } else if (*p == '+' || *p == '-' || *p == '*' || *p == '/' ||
+                   *p == '(' || *p == ')') {
             cur = new_token(TK_RESERVED, cur, p++);
         } else if (isdigit(*p)) {
             cur = new_token(TK_NUM, cur, p);
@@ -100,6 +102,96 @@ Token *tokenize() {
     return head.next;
 }
 
+typedef enum { ND_ADD, ND_SUB, ND_MUL, ND_DIV, ND_NUM } NodeKind;
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+/* EBNF:
+ *   expr    = mul | (("+" | "-") mul)*
+ *   mul     = primary | (("*" | "/") primary)*
+ *   primary = num | "(" expr ")"
+ */
+
+Node *expr();
+Node *primary() {
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+    return new_node_num(expect_number());
+}
+
+Node *mul() {
+    Node *node = primary();
+    while (true) {
+        if (consume('*'))
+            node = new_node(ND_MUL, node, primary());
+        else if (consume('/'))
+            node = new_node(ND_DIV, node, primary());
+        else
+            return node;
+    }
+}
+
+Node *expr() {
+    Node *node = mul();
+    while (true) {
+        if (consume('+'))
+            node = new_node(ND_ADD, node, mul());
+        else if (consume('-'))
+            node = new_node(ND_SUB, node, mul());
+        else
+            return node;
+    }
+}
+
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+    if (node->kind == ND_ADD) {
+        printf("  add rax, rdi\n");
+    } else if (node->kind == ND_SUB) {
+        printf("  sub rax, rdi\n");
+    } else if (node->kind == ND_MUL) {
+        printf("  imul rax, rdi\n");
+    } else if (node->kind == ND_DIV) {
+        printf("  cqo\n");
+        printf("  idiv rdi\n");
+    } else {
+        assert(false);
+    }
+    printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         error("the number of arguments is wrong.");
@@ -107,21 +199,14 @@ int main(int argc, char **argv) {
 
     user_input = argv[1];
     token = tokenize();
+    Node *node = expr();
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
 
-    printf("  mov rax, %d\n", expect_number());
+    gen(node);
 
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
-        expect('-');
-        printf("  sub rax, %d\n", expect_number());
-    }
-
+    printf("  pop rax\n");
     printf("  ret\n");
 }
